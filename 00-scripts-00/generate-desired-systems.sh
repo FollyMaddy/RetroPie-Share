2f21e1f01cd88127358cfc712ec1cd6aacfe447e
FollyMaddy <meulenf@gmx.net>
2020-11-30 21:42:50 +0100

    Add files via upload

--- 00-scripts-00/generate-desired-systems.sh
+++ 00-scripts-00/generate-desired-systems.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # use 1 or more lines to create one system or more systems
+# by uncommenting -> remove #
 
 #bash generate-lr-mess-systems.sh x1 # bootable
 #bash generate-lr-mess-systems.sh mz2500 # bootable
@@ -8,8 +9,14 @@
 #bash generate-lr-mess-systems.sh pc6001 # bootable
 #bash generate-lr-mess-systems.sh pc6001mk2 # bootable
 #bash generate-lr-mess-systems.sh zx81 # bootable
+#bash generate-lr-mess-systems.sh smc777 # bootable
 #bash generate-lr-mess-systems.sh videopac #videopac g7000 # bios files missing
 #bash generate-lr-mess-systems.sh g7400 #videopac g7400 # bios files missing
+
+#bash generate-lr-mess-systems.sh mtx512 # bios files missing
+
+#amiga
+#bash generate-lr-mess-systems.sh a500 # bootable (needs extra keyboard rom), sometimes sluggish, not always good sound, mouse not working oob
 
 #msx systems
 #bash generate-lr-mess-systems.sh fsa1wsx # bootable # FS-A1WX / 1st released version (Japan) (MSX2+)
