cd1508c7aec4335d5ee005ea0ee8d33c6cb2f683
FollyMaddy <meulenf@gmx.net>
2021-02-15 20:56:28 +0100

    Add files via upload

--- 00-scripts-00/generate-desired-systems.sh
+++ 00-scripts-00/generate-desired-systems.sh
@@ -26,18 +26,18 @@ version=generate-systems-lr-mess_mame-1v8-alpha.sh
 #bash $version g7400 #videopac g7400 # bios files missing
 #bash $version apple2gs # bootable
 #bash $version mtx512 # bios files missing
-#bash $version vectrex 
-#bash $version nes 
+bash $version vectrex 
+bash $version nes 
 
 #handhelds (use version 1v4-alpha or above)(creates only cmd scripts)(these systems have no external media)
 ##such a cmd script can be created with one game and used for many ! 
-#bash $version ablmini # creates cmd script for all_in1 # bootable 
-#bash $version alnattck # creates cmd script for classich # bootable
-#bash $version gnw_ball # creates cmd script for gameandwatch # bootable 
-#bash $version jak_batm # creates cmd script for jakks # bootable
-#bash $version kgradius # creates cmd script for konamih # bootable
-#bash $version taddams # creates cmd script for tigerh # bootable
-#bash $version rzbatfor # creates cmd script for tigerrz # bootable
+bash $version ablmini # creates cmd script for all_in1 # bootable 
+bash $version alnattck # creates cmd script for classich # bootable
+bash $version gnw_ball # creates cmd script for gameandwatch # bootable 
+bash $version jak_batm # creates cmd script for jakks # bootable
+bash $version kgradius # creates cmd script for konamih # bootable
+bash $version taddams # creates cmd script for tigerh # bootable
+bash $version rzbatfor # creates cmd script for tigerrz # bootable
 
 #amiga
 #bash $version a500 # bootable (needs extra keyboard rom), sometimes sluggish, not always good sound, mouse not working oob
