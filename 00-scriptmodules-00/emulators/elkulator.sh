0d6aca859d3089cd19548b953c48809efd18ed70
FollyMaddy <meulenf@gmx.net>
2021-06-05 19:07:36 +0200

    add fullscreen and auto media

--- 00-scriptmodules-00/emulators/elkulator.sh
+++ 00-scriptmodules-00/emulators/elkulator.sh
@@ -16,7 +16,7 @@ rp_module_section="exp"
 rp_module_flags=""
 
 function depends_elkulator() {
-    getDepends xorg matchbox-window-manager automake liballegro4-dev zlib1g-dev libalut-dev libopenal-dev autotools-dev
+    getDepends xorg matchbox-window-manager automake liballegro4-dev zlib1g-dev libalut-dev libopenal-dev autotools-dev xdotool
 }
 
 function sources_elkulator() {
@@ -46,33 +46,39 @@ _EOF_
 sed -i 's/win_resize = 0/win_resize = 1/g' $md_inst/elk.cfg
 sed -i 's/plus3 = 0/plus3 = 1/g' $md_inst/elk.cfg
 
-    cat >"$md_inst/elkulator-rom1.sh" << _EOF_
+    cat >"$md_inst/elkulator-multiload.sh" << _EOF_
 #!/bin/bash
+#see for keylist codes => https://gitlab.com/cunidev/gestures/-/wikis/xdotool-list-of-key-codes
+#see here for tips and tricks ==> https://github.com/damieng/BBCMicroTools/blob/master/tips-and-tricks.md
+#if using a us keyboard you have to use "quotedbl" to get "*" and "at" to get '"', don't know, perhaps there is a better way
+function load_rom() {
+fullscreen=();fullscreen=( "F11" "Alt+s" "v" "Down" "Down" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
-/opt/retropie/emulators/elkulator/elkulator -rom1 "\$1"
-_EOF_
-    chmod +x "$md_inst/elkulator-rom1.sh"
-
-    cat >"$md_inst/elkulator-tape.sh" << _EOF_
-#!/bin/bash
+/opt/retropie/emulators/elkulator/elkulator -rom1 "\$1"|\
+for index in \${!fullscreen[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen[\$index]} sleep 0.05 keyup \${fullscreen[\$index]};done
+}
+function load_tape() {
+fullscreen_cassload=();fullscreen_cassload=( "F11" "Alt+s" "v" "Down" "Down" "Return" "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
-/opt/retropie/emulators/elkulator/elkulator -tape "\$1"
-_EOF_
-    chmod +x "$md_inst/elkulator-tape.sh"
-
-    cat >"$md_inst/elkulator-disc.sh" << _EOF_
-#!/bin/bash
+/opt/retropie/emulators/elkulator/elkulator -tape "\$1"|\
+for index in \${!fullscreen_cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen_cassload[\$index]} sleep 0.1 keyup \${fullscreen_cassload[\$index]};done
+}
+function load_disc() {
+fullscreen_discload=();fullscreen_discload=( "F11" "Alt+s" "v" "Down" "Down" "Return" "quotedbl" "e" "x" "e" "c" "Space" "exclam" "b" "o" "o" "t" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
-/opt/retropie/emulators/elkulator/elkulator -disc "\$1"
+/opt/retropie/emulators/elkulator/elkulator -disc "\$1"|\
+for index in \${!fullscreen_discload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen_discload[\$index]} sleep 0.1 keyup \${fullscreen_discload[\$index]};done
+}
+[[ "\$1" == *.rom ]] && load_rom "\$1"
+[[ "\$1" == *.uef ]] && load_tape "\$1"
+[[ "\$1" == *.ssd ]] && load_disc "\$1"
 _EOF_
-    chmod +x "$md_inst/elkulator-disc.sh"
+    chmod +x "$md_inst/elkulator-multiload.sh"
 
     mkRomDir "electron"
-    addEmulator 0 "elkulator-rom1" "electron" "XINIT:$md_inst/elkulator-rom1.sh %ROM%"
-    addEmulator 0 "elkulator-tape" "electron" "XINIT:$md_inst/elkulator-tape.sh %ROM%"
-    addEmulator 0 "elkulator-disc" "electron" "XINIT:$md_inst/elkulator-disc.sh %ROM%"
+    addEmulator 0 "elkulator-multiload" "electron" "XINIT:$md_inst/elkulator-multiload.sh %ROM%"
     addSystem "electron" "Acorn Electron" ".rom .uef .ssd"
 }
