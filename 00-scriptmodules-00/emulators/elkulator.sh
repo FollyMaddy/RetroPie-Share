a2d8e04ebca5beb103355630937fe310134386ed
FollyMaddy <meulenf@gmx.net>
2021-06-05 22:31:28 +0200

    on the fly cfg patching for adfs support

--- 00-scriptmodules-00/emulators/elkulator.sh
+++ 00-scriptmodules-00/emulators/elkulator.sh
@@ -39,12 +39,21 @@ function install_elkulator() {
 }
 
 function configure_elkulator() {
+
+rm "$md_inst/elk.cfg" 2>&-
+cp "$md_inst/elk.cfg.bak" "$md_inst/elk.cfg" 2>&-
+
+cp "$md_inst/elk.cfg" "$configdir/electron/elk.cfg"
+sed -i 's/win_resize = 0/win_resize = 1/g' "$configdir/electron/elk.cfg"
+sed -i 's/plus3 = 0/plus3 = 1/g' "$configdir/electron/elk.cfg"
+chown $user:$user "$configdir/electron/elk.cfg"
+
+mv "$md_inst/elk.cfg" "$md_inst/elk.cfg.bak"
+ln -s "$configdir/electron/elk.cfg" "$md_inst/elk.cfg"
+
     cat >"$md_inst/matchbox_key_shortcuts" << _EOF_
 <ctrl>c=close
 _EOF_
-
-sed -i 's/win_resize = 0/win_resize = 1/g' $md_inst/elk.cfg
-sed -i 's/plus3 = 0/plus3 = 1/g' $md_inst/elk.cfg
 
     cat >"$md_inst/elkulator-multiload.sh" << _EOF_
 #!/bin/bash
@@ -52,6 +61,7 @@ sed -i 's/plus3 = 0/plus3 = 1/g' $md_inst/elk.cfg
 #see here for tips and tricks ==> https://github.com/damieng/BBCMicroTools/blob/master/tips-and-tricks.md
 #if using a us keyboard you have to use "quotedbl" to get "*" and "at" to get '"', don't know, perhaps there is a better way
 function load_rom() {
+sed -i 's/adfsena = 1/adfsena = 0/g' "$configdir/electron/elk.cfg"
 fullscreen=();fullscreen=( "F11" "Alt+s" "v" "Down" "Down" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
@@ -59,14 +69,25 @@ matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matc
 for index in \${!fullscreen[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen[\$index]} sleep 0.05 keyup \${fullscreen[\$index]};done
 }
 function load_tape() {
+sed -i 's/adfsena = 1/adfsena = 0/g' "$configdir/electron/elk.cfg"
 fullscreen_cassload=();fullscreen_cassload=( "F11" "Alt+s" "v" "Down" "Down" "Return" "quotedbl" "t" "a" "p" "e" "Return" "c" "h" "a" "i" "n" "at" "at" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
 /opt/retropie/emulators/elkulator/elkulator -tape "\$1"|\
 for index in \${!fullscreen_cassload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen_cassload[\$index]} sleep 0.1 keyup \${fullscreen_cassload[\$index]};done
 }
-function load_disc() {
+function load_disc_dfs() {
+sed -i 's/adfsena = 1/adfsena = 0/g' "$configdir/electron/elk.cfg"
 fullscreen_discload=();fullscreen_discload=( "F11" "Alt+s" "v" "Down" "Down" "Return" "quotedbl" "e" "x" "e" "c" "Space" "exclam" "b" "o" "o" "t" "Return" )
+xset -dpms s off s noblank
+matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
+/opt/retropie/emulators/elkulator/elkulator -disc "\$1"|\
+for index in \${!fullscreen_discload[@]};do xdotool \$(if [[ \$index == 0 ]];then echo "sleep 1.5";fi) keydown \${fullscreen_discload[\$index]} sleep 0.1 keyup \${fullscreen_discload[\$index]};done
+}
+function load_disc_adfs() {
+sed -i 's/adfsena = 0/adfsena = 1/g' "$configdir/electron/elk.cfg"
+#adfs autoload is not implemented yet
+fullscreen_discload=();fullscreen_discload=( "F11" "Alt+s" "v" "Down" "Down" "Return" )
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no -use_cursor no -kbdconfig $md_inst/matchbox_key_shortcuts &
 /opt/retropie/emulators/elkulator/elkulator -disc "\$1"|\
@@ -74,11 +95,12 @@ for index in \${!fullscreen_discload[@]};do xdotool \$(if [[ \$index == 0 ]];the
 }
 [[ "\$1" == *.rom ]] && load_rom "\$1"
 [[ "\$1" == *.uef ]] && load_tape "\$1"
-[[ "\$1" == *.ssd ]] && load_disc "\$1"
+[[ "\$1" == *.ssd ]] && load_disc_dfs "\$1"
+[[ "\$1" == *.adf ]] && load_disc_adfs "\$1"
 _EOF_
     chmod +x "$md_inst/elkulator-multiload.sh"
 
     mkRomDir "electron"
     addEmulator 0 "elkulator-multiload" "electron" "XINIT:$md_inst/elkulator-multiload.sh %ROM%"
-    addSystem "electron" "Acorn Electron" ".rom .uef .ssd"
+    addSystem "electron" "Acorn Electron" ".rom .uef .ssd .adf"
 }
