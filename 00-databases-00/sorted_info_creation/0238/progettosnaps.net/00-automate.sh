8630a45ccc23413eafb66a3820adf02849276174
FollyMaddy <meulenf@gmx.net>
2021-12-12 18:59:09 +0100

    good line for tbaskb

--- 00-databases-00/sorted_info_creation/0238/progettosnaps.net/00-automate.sh
+++ 00-databases-00/sorted_info_creation/0238/progettosnaps.net/00-automate.sh
@@ -61,6 +61,6 @@
  ##for file in $(ls -r *.ini);do sed -i "s/@$(basename $file .ini)@$(basename $file .ini)/@$(basename $file .ini)/g" mame0$1_systems_sorted_info;done
  ##for file in $(ls -r *.ini);do sed -i "s/@$(basename $file .ini)@$(basename $file .ini)/@$(basename $file .ini)/g" mame0$1_systems_sorted_info;done
  
- #add this 0229 manually
- #Driver tbaskb (Electronic Basketball (Tandy)): @classich@electronic_game@handheld@non-arcade
+ #add this 0229 manually (seems to be a bit problematic but this line should work)
+ #Driver tbaskb (Electronic Basketball (Tandy)): @last0229@classich@electronic_game@handheld@non-arcade
  fi
