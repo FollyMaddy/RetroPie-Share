e23e2a058872b2a8c05f391ef4020c8bcde092e1
FollyMaddy <meulenf@gmx.net>
2022-08-05 17:38:34 +0200

    fix error when selecting systems
    Issue is described here :
    https://retropie.org.uk/forum/topic/29682/development-of-module-script-generator-for-lr-mess-lr-mame-and-mame-standalone/1172

--- 00-scriptmodules-00/supplementary/add-mamedev-systems.sh
+++ 00-scriptmodules-00/supplementary/add-mamedev-systems.sh
@@ -182,7 +182,7 @@ function subgui_add-mamedev-systems_all() {
     csv=(
 ",menu_item,,to_do,,,,,help_to_do,"
 ",Display all upon descriptions,,choose_add descriptions,,,,,dialog_message \"Install one or more systems with default options\","
-",Display all upon system names,,choose_add,,,,,dialog_message \"Install one or more systems with default options\","
+",Display all upon system names,,choose_add systems,,,,,dialog_message \"Install one or more systems with default options\","
 ",,,,,,,,,"
 ",Display alphabetical submenu upon descriptions,,subgui_add-mamedev-systems_alphabetical_order_selection descriptions,,,,,dialog_message \"Select a list and then install one or more systems with default options\","
 ",Display alphabetical submenu upon system names,,subgui_add-mamedev-systems_alphabetical_order_selection systems,,,,,dialog_message \"Select a list and then install one or more systems with default options\","
@@ -788,7 +788,7 @@ function build_menu_add-mamedev-systems() {
     unset 'options[0]'; unset 'options[1]' 
     while true; do
         local cmd=(dialog --colors --no-collapse --help-button --default-item "$default" --backtitle "$__backtitle" --menu "What would you like to select or install ?	\
-	Version 0245.03" 22 76 16)
+	Version 0245.04" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         if [[ -n "$choice" ]]; then
