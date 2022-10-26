7b64947e2f8d70f7ce29089a8366a44b47e69315
FollyMaddy <meulenf@gmx.net>
2022-10-26 17:44:30 +0200

    add shader dependency, path and help
    Normally the shaders from the master branch are not installed on a rpi.
    Though the gb-pocket-shader will work on the rpi4. (others not tested)
    To get it working these shaders are cloned into the shaders-extra folder.
    Now the can be used without interfering with the standard shaders installed on a rpi.

--- 00-scriptmodules-00/libretrocores/lr-arduous_lcd.sh
+++ 00-scriptmodules-00/libretrocores/lr-arduous_lcd.sh
@@ -9,18 +9,28 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="lr-arduous"
-rp_module_desc="ArduBoy emulator - arduous port for libretro"
-rp_module_help="ROM Extensions: .ghex .zip\n\nCopy your ArduBoy roms to $romdir/arduboy"
+rp_module_id="lr-arduous_lcd"
+rp_module_desc="ArduBoy emulator with LCD shader - arduous port for libretro"
+rp_module_help="ROM Extensions: .hex .zip\n\nCopy your ArduBoy roms to $romdir/arduboy\n\nThis version uses the gb-pocket-shader from the master branch of the common-shader repository.\nThese shaders are cloned into :\n/opt/retropie/configs/all/retroarch/shaders-extra/.\nThe specific shader is added to load from :\n/opt/retropie/configs/arduboy/retroarch.cfg.\nBe aware that when installing another lr-arduous port the shader setting will probably be removed in :\n/opt/retropie/configs/arduboy/retroarch.cfg."
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/arduous/master/license.txt"
 rp_module_repo="git https://github.com/libretro/arduous main"
 rp_module_section="exp"
 
-function sources_lr-arduous() {
+function depends_lr-arduous_lcd() {
+    local dir="$configdir/all/retroarch/shaders-extra"
+    local branch=""
+    branch="master"
+    # remove if not git repository for fresh checkout
+    [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
+    gitPullOrClone "$dir" https://github.com/RetroPie/common-shaders.git "$branch"
+    chown -R $user:$user "$dir"
+}
+
+function sources_lr-arduous_lcd() {
     gitPullOrClone
 }
 
-function build_lr-arduous() {
+function build_lr-arduous_lcd() {
     cd build
     cmake ..
     make clean
@@ -28,20 +38,20 @@ function build_lr-arduous() {
     md_ret_require="$md_build/build/arduous_libretro.so"
 }
 
-function install_lr-arduous() {
+function install_lr-arduous_lcd() {
     md_ret_files=(
         'build/arduous_libretro.so'
     )
 }
 
-function configure_lr-arduous() {
+function configure_lr-arduous_lcd() {
     mkRomDir "arduboy"
 
     ensureSystemretroconfig "arduboy"
     
     # add the per system default settings
     iniConfig " = " '"' "$configdir/arduboy/retroarch.cfg"
-    iniSet "video_shader" "$configdir/all/retroarch/shaders/handheld/gameboy/gb-pocket-shader.glslp"
+    iniSet "video_shader" "$configdir/all/retroarch/shaders-extra/handheld/gameboy/gb-pocket-shader.glslp"
 
     addEmulator 1 "$md_id" "arduboy" "$md_inst/arduous_libretro.so"
 
