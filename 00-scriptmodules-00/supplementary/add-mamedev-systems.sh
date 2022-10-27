cf02bc148f8e08f58f4a9471f1b82df277787958
FollyMaddy <meulenf@gmx.net>
2022-10-27 16:15:23 +0200

    version change to 248.04
    add "hard" for installing harddrive runcommands
    was missing for aa5000

--- 00-scriptmodules-00/supplementary/add-mamedev-systems.sh
+++ 00-scriptmodules-00/supplementary/add-mamedev-systems.sh
@@ -800,7 +800,7 @@ function build_menu_add-mamedev-systems() {
     unset 'options[0]'; unset 'options[1]' 
     while true; do
         local cmd=(dialog --colors --no-collapse --help-button --default-item "$default" --backtitle "$__backtitle" --menu "What would you like to select or install ?	\
-	Version 0248.03" 22 76 16)
+	Version 0248.04" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         if [[ -n "$choice" ]]; then
@@ -910,7 +910,7 @@ newsystems=()
 namesfilter="\(brief|------"
 
 #filter on usefull media, otherwise we also get many unusefull scripts
-mediafilter="none\)|\(prin|quik\)|\(memc|\(rom1|\(cart|\(flop|\(cass|dump\)|cdrm\)|\(hard1|\(min|\(mout"
+mediafilter="none\)|\(prin|quik\)|\(memc|\(rom1|\(cart|\(flop|\(cass|dump\)|cdrm\)|\(hard|\(hard1|\(min|\(mout"
 
 #string for adding extra extensions in all generated scripts
 addextensions=".zip .7z"
