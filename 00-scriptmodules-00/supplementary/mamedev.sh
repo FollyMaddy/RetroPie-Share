b10ac0e7003b9e427e490a1970dfe32367801d3f
FollyMaddy <meulenf@gmx.net>
2025-04-13 12:12:41 +0200

    version change to 276.00

--- 00-scriptmodules-00/supplementary/mamedev.sh
+++ 00-scriptmodules-00/supplementary/mamedev.sh
@@ -25,7 +25,7 @@ rp_module_desc="Add MAME/lr-mame/lr-mess systems"
 rp_module_section="config"
 
 rp_module_build="Default"
-rp_module_version="0275.00"
+rp_module_version="0276.00"
 rp_module_version_database="${rp_module_version%.*}"
 if [[ -f $emudir/mame/mame ]];then
  #works in terminal but not here ?
